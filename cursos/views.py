085657e9ba18baa062b7ee682977da6fc974302b
VictorDiazOsorio <victor.diazosorio@gmail.com>
2020-04-21 17:02:41 -0400

    formulario de contactos funcionando

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Taller, Noticia
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
 # Create your views here.
 
 def listadoDeCursos( request ):
@@ -11,6 +15,22 @@ def listadoDeCursos( request ):
     return render( request, 'cursos/listadoCursos.html', contexto )
 
 def formularioContacto( request ):
+    if request.method == 'POST':
+        autor = request.POST['Nombre']
+        correo = request.POST['Email']
+        mensaje = request.POST['Mensaje']
+        mensaje = "Nombre: " + autor + " Correo: " + correo + " Mensaje: " + mensaje
+
+        send_mail( 
+            autor + ' Formulario de contacto', 
+            mensaje, 
+            settings.EMAIL_HOST_USER, 
+            ['escuelaether@gmail.com'], 
+            fail_silently = False )
+        
+        messages.success( request, 'Mensaje recibido, te responderemos a la brevedad!' )
+        return redirect( '../../' )
+
     return render( request, 'cursos/formularioContacto.html' )
 
 def homeView( request ):
--- eter/settings.py
+++ eter/settings.py
@@ -123,3 +123,17 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+# Email config
+
+EMAIL_HOST = 'smtp.gmail.com'
+##############
+#EMAIL_HOST_USER = 
+
+#EMAIL_HOST_PASSWORD = 
+##############
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
